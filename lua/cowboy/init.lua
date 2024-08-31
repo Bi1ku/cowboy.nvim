615cae420a9390c02685be9d226c1926179c46e5
Bi1ku <2008owenshi@gmail.com>
2024-08-31 08:03:59 -0400

    remove utils import

--- lua/cowboy/init.lua
+++ lua/cowboy/init.lua
@@ -1,5 +1,3 @@
-local utils = require("cowboy.utils")
-
 local cowboy = {}
 
 local count = 0
@@ -7,35 +5,35 @@ local prev_key = ""
 local options = {}
 
 local handler = function(key)
-	vim.keymap.set("n", key, function()
-		count = count + 1
+  vim.keymap.set("n", key, function()
+    count = count + 1
 
-		if prev_key ~= key then
-			count = 0
-		end
+    if prev_key ~= key then
+      count = 0
+    end
 
-		if count > options.thresh then
-			vim.notify("Slow down, Cowboy!", vim.log.levels.WARN)
-			vim.defer_fn(function()
-				count = 0
-				print("reset", count, options.thresh, count > options.thresh)
-				return key
-			end, options.delay)
-		else
-			return key
-		end
-	end, { expr = true, silent = true })
+    if count > options.thresh then
+      vim.notify("Slow down, Cowboy!", vim.log.levels.WARN)
+      vim.defer_fn(function()
+        count = 0
+        print("reset", count, options.thresh, count > options.thresh)
+        return key
+      end, options.delay)
+    else
+      return key
+    end
+  end, { expr = true, silent = true })
 end
 
 cowboy.setup = function(opts)
-	options = opts
-	for _, key in ipairs(opts.keys) do
-		handler(key)
-	end
+  options = opts
+  for _, key in ipairs(opts.keys) do
+    handler(key)
+  end
 
-	vim.on_key(function(key)
-		prev_key = key
-	end)
+  vim.on_key(function(key)
+    prev_key = key
+  end)
 end
 
 return cowboy
